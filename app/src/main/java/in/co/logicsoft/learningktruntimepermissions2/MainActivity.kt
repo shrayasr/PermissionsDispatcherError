15e4b03735d4d82601b2dcec28526d91b776173b
Shrayas <shrayasr@gmail.com>
2018-05-05 15:58:44 +0530

    Initial commit

---
+++ app/src/main/java/in/co/logicsoft/learningktruntimepermissions2/MainActivity.kt
@@ -0,0 +1,54 @@
+package `in`.co.logicsoft.learningktruntimepermissions2
+
+import android.Manifest
+import android.os.Bundle
+import android.support.annotation.StringRes
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import permissions.dispatcher.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_camera.setOnClickListener {
+            showCameraWithPermissionCheck()
+        }
+    }
+
+    @NeedsPermission(Manifest.permission.CAMERA)
+    fun showCamera() {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.sample_content_fragment, CameraPreviewFragment.newInstance())
+                .addToBackStack("camera")
+                .commitAllowingStateLoss()
+    }
+
+    @OnShowRationale(Manifest.permission.CAMERA)
+    fun showRationaleForCamera(request: PermissionRequest) {
+        showRationaleDialog(R.string.permission_camera_rationale, request)
+    }
+
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    fun onCameraDenied() {
+        Toast.makeText(this, R.string.permission_camera_denied, Toast.LENGTH_SHORT).show()
+    }
+
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    fun onCameraNeverAskAgain() {
+        Toast.makeText(this, R.string.permission_camera_never_ask_again, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showRationaleDialog(@StringRes messageResId: Int, request: PermissionRequest) {
+        AlertDialog.Builder(this)
+                .setPositiveButton(R.string.button_allow) { _, _ -> request.proceed() }
+                .setNegativeButton(R.string.button_deny) { _, _ -> request.cancel() }
+                .setCancelable(false)
+                .setMessage(messageResId)
+                .show()
+    }
+}
